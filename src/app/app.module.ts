2da7dddc720bf2a133ca2482d6dcba6cdfd4734b
Juan Carlos <juancarlos.rmr@gmail.com>
2021-03-20 09:59:25 +0100

    feat: configura conexión a firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 import { PrincipalModule } from './components/principal/principal.module';
 
 // Components
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './components/principal/principal.component';
+import { UsersModule } from './users/users.module';
 
 @NgModule({
   declarations: [AppComponent, PrincipalComponent],
@@ -17,6 +20,8 @@ import { PrincipalComponent } from './components/principal/principal.component';
     PrincipalModule,
     AuthModule,
     UsersModule,
+    AngularFireModule.initializeApp(environment.apis.dutti.firebase),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
