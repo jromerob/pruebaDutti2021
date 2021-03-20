488fc8c48c4d5a496d4eb7244b464a32b344235b
Juan Carlos <juancarlos.rmr@gmail.com>
2021-03-20 08:06:05 +0100

    refactor: creación módulo auth y refactorización componentes
    Integra en módulo auth los componentes de login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/components/login/login.component';
 // Components
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+
+import { RegisterComponent } from './auth/components/register/register.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent},
-  { path: 'register', component: RegisterComponent},
-  { path: 'principal', loadChildren: () => import(`./components/principal/principal.module`).then(m => m.PrincipalModule) }
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'principal',
+    loadChildren: () =>
+      import(`./components/principal/principal.module`).then(
+        (m) => m.PrincipalModule
+      ),
+  },
   // { path: 'ships', loadChildren: () => import(`./components/ships/ships.module`).then(m => m.ShipsModule) }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
