42aadb984fd38822e186d1fd8e4b58e8a1dec0c7
Juan Carlos <juancarlos.rmr@gmail.com>
2021-03-21 20:26:11 +0100

    feat: Despacha desde el componente la carga inicial de ships

--- src/app/components/ships/ships.component.ts
+++ src/app/components/ships/ships.component.ts
@@ -1,9 +1,10 @@
 import { ShipsResponse } from './../../ships/models/ships-response.model';
 import { Component, OnInit } from '@angular/core';
-import { ShipsService } from 'src/app/ships/services/ships.service';
+import { ShipsService } from '../../ships/services/ships.service';
 import { Store } from '@ngrx/store';
 import { AppState } from './../../app.state';
 import { Observable } from 'rxjs';
+import * as ShipsActions from '../../ships/store/ships.actions';
 
 @Component({
   selector: 'app-ships',
@@ -12,21 +13,15 @@ import { Observable } from 'rxjs';
 })
 export class ShipsComponent implements OnInit {
   public shipsResponse: ShipsResponse;
-  // Estdo de la consulta de ships
+  // Estado de la consulta de ships
   public shipsState$: Observable<ShipsResponse>;
 
-  constructor(
-    private shipsService: ShipsService,
-    private store: Store<AppState>
-  ) {
+  constructor(private store: Store<AppState>) {
     // Observable del Store
     this.shipsState$ = this.store.select('ships');
   }
 
   ngOnInit(): void {
-    this.shipsService.getShips().subscribe((shipsResponse) => {
-      this.shipsResponse = shipsResponse;
-      console.log('SHIPS -->', this.shipsResponse.results);
-    });
+    this.store.dispatch({ type: ShipsActions.GET_SHIPS });
   }
 }
